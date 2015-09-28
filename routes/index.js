351b411145a8b531fa8c2ed524db36b386198613
Scott <scotteggs@gmail.com>
2015-09-28 18:01:48 -0400

    updates to allow database driven views:

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,25 @@ var express = require('express')
 
 var router = express.Router()
 
-var tweetbank = require('../tweetbank')
+// var tweetbank = require('../tweetbank')
+
+var models = require('../models/index.js')
+
 
 module.exports = router
 
 // home
 router.get('/', function(req, res, next) {
-  res.render('index', { tweets: tweetbank.list(), showForm:true })
-})
+
+	models.Tweet.findAll({ include: [models.User] }).then(function (tweets) {
+		console.log(JSON.stringify(tweets, null, 2))
+		res.render('index', { tweets: tweets })
+	})
+});
+
+
+
+
 
 // make a tweet
 router.post('/', function(req, res, next) {
@@ -17,12 +28,45 @@ router.post('/', function(req, res, next) {
   res.redirect('/')
 })
 
-// getting all tweets from user
+// // getting a tweet from a user
+// router.get('/users/:name', function(req, res, next) {
+// 	models.User.findOne({where: {name: req.params.name}}).then(function (user) {
+//     return user.getTweets();
+// 	})
+// 	.then(function (tweets) {
+//     JSON.stringify(tweets); // another way of just logging the plain old values
+// 	});
+//    res.json(tweets)
+
+//   //res.render('index', { tweets: tweets })
+// })
+
+//getting all tweets from a user
 router.get('/users/:name', function(req, res, next) {
-  var tweets = tweetbank.find(req.params)
-  // res.json(tweets)
-  res.render('index', { tweets: tweets })
+	var u;
+	models.User.findOne({ where: {name: req.params.name}}).then(function (user) {
+    u = user;
+    console.log('user', user.get({plain: true}))
+    return user.getTweets();
+	})
+	.then(function (tweets) {
+		
+		tweets = tweets.map(function (tweet) {
+			var tweetObj = tweet.get({plain: true});
+			tweetObj.User = u;
+			return tweetObj;
+		})
+    //console.log('tweet', JSON.stringify(tweets))
+    //JSON.stringify(tweets); // another way of just logging the plain old values
+    //res.json(tweets)
+    res.render('index', { tweets: tweets })
+	});
+
+  //
 })
+
+
+
 
 // get a single tweet
 router.get('/users/:name/tweets/:id', function(req, res, next) {
@@ -30,13 +74,5 @@ router.get('/users/:name/tweets/:id', function(req, res, next) {
   var tweets = tweetbank.find(req.params)
   res.render('index',{ tweets: tweets})
 })
-
-
-
-
-
-
-
-
 
 
